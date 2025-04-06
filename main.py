8f73ab3bc36ce5e4dd898fed1b28a266034df2fc
mariocooper00 <mariocooper00@hotmail.com>
2025-04-06 14:42:53 +0100

    update books and enhance functionality

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import (
     get_num_words,
     char_count,
@@ -6,7 +8,11 @@ from stats import (
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = char_count(text)
